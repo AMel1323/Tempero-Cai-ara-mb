71da69c419c312ee7ae099d45d4ef14594c3b9b6
geovannaetm <geovanna.t@aluno.ifsp.edu.br>
2025-09-18 11:05:08 -0300

    criando tabs

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -1,39 +1,66 @@
 import { Tabs } from "expo-router"
 import Foundation from '@expo/vector-icons/Foundation'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Entypo from '@expo/vector-icons/Entypo'
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 export default function RootLayout() {
     return(
-        <Tabs screenOptions={{
-            tabBarActiveTintColor: '#ff7b00',
-            tabBarInactiveTintColor: '#2b2b2bff',
-            tabBarLabelStyle: { fontSize: 8 },
-        }}
-        >
+        <Tabs
+  screenOptions={{
+    tabBarShowLabel: false,
+    tabBarActiveTintColor: '#fff', // cor ativa (laranja)
+    tabBarInactiveTintColor: '#fff',  // cor inativa (branca)
+   
+    tabBarStyle: {
+     
+      position: 'absolute',
+      bottom: 40,       // espaçamento da borda inferior
+      left: 20,
+      right: 20,
+      borderRadius: 25, // bordas arredondadas
+      height: 60,       // altura da TabBar
+      backgroundColor: '#2c6e91', // azul da sua tela
+      borderTopWidth: 0, // tira borda padrão
+      elevation: 5,      // sombra no Android
+      shadowColor: '#000', // sombra no iOS
+      shadowOpacity: 0.4,
+      shadowRadius: 5,
+    
+    },
+    tabBarHideOnKeyboard: true, // esconde quando teclado abre
+    headerShown: false,
+  }}
+>
+  <Tabs.Screen
+    name="home"
+    options={{
+     
+      tabBarIcon: ({ color }) => (
+        <Ionicons name="home-outline" size={25} color={color} style={{ marginTop: 15 }}/>
+      ),
+    }}
+  />
 
-        <Tabs.Screen 
-                name="home"
-                options={{ 
-                    title: "Home",
-                    //tabBarShowLabel: false,
-                    headerShown: false,
-                    tabBarIcon: ({color}) => <Foundation name="home" size={24} color={color} /> 
-                }}
-            />
+
+
+<Tabs.Screen
+    name="pedido"
+    options={{
+     
+      tabBarIcon: ({ color }) => (
+        <SimpleLineIcons name="book-open" size={25} color={color} style={{ marginTop: 15 }}/>
+      ),
+    }}
+  />
+
+  
+ 
 
 
 
-
-
-
-
-
-
-
-        </Tabs>
-
+</Tabs>
 
     )
 }
--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,16 +1,44 @@
-import { Image } from 'expo-image'
-import { View, Text, StyleSheet } from 'react-native'
-import Entypo from '@expo/vector-icons/Entypo'
+import { Image } from "expo-image";
+import { View, Text, StyleSheet } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
 
-
-export default function Header() {
-    return(
-        <View>
-            
-
-
-
-        </View>
-
-    )
+export default function Topo() {
+  return (
+    <View style={styles.header}>
+      <View style={styles.logotipo}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/img/logo.png")}
+        />
+      </View>
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  header: {
+    top: 0,
+    width: "100%",
+    height: 130,
+    backgroundColor: "#296D8C",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    position: "fixed",
+    flex:1
+
+   
+  },
+
+  image: { 
+    width: 230, 
+    height: 90
+  },
+  logotipo:{
+    
+    alignItems: "center",
+    gap: 10
+  },
+ 
+}) 
